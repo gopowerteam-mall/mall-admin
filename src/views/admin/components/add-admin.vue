9bb5b87cfe720d9d228ab6c2294dae9eb74d0e90
huangrui <hr837@163.com>
2022-08-19 19:30:22 +0800

    feat(none): 完成管理员列表页面
    ISSUES CLOSED: #2

---
+++ src/views/admin/components/add-admin.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+a-form(:model='form' :rules='formRules' @submit-success='handleSubmit')
+  a-form-item(field='username' label='账号')
+    a-input(v-model='form.username' allow-clear placeholder='请输入账号')
+  a-form-item(field='password' label='密码')
+    a-input(
+      v-model='form.password'
+      allow-clear
+      autocomplete='new-password'
+      placeholder='请输入登录密码'
+      type='password')
+  a-form-item(content-class='!justify-end')
+    a-button(@click='onCancel') 取消
+    a-button.m-l-4(:loading='saving' html-type='submit' type='primary') 提交
+</template>
+
+<script lang="ts" setup>
+import { Message } from '@arco-design/web-vue'
+import { useRequest } from 'virtual:http-request'
+
+const form = $ref({
+  username: '',
+  password: '',
+})
+
+const formRules = {
+  username: { required: true, message: '请输入账号' },
+  password: { required: true, message: '请输入密码' },
+}
+
+let saving = $ref(false)
+
+const emits = defineEmits(['close', 'success'])
+
+function onCancel() {
+  emits('close')
+}
+
+function onSuncess() {
+  Message.success('添加成功')
+  emits('success')
+  saving = false
+  onCancel()
+}
+
+function handleSubmit() {
+  saving = true
+  useRequest((service) => service.AdminService)
+    .createAdmin(form)
+    .subscribe({
+      next: onSuncess,
+      error: () => (saving = false),
+    })
+}
+</script>
---
+++ src/views/admin/index.vue
@@ -0,0 +1,157 @@
+<template lang="pug">
+page-container(title='管理员列表')
+  a-table.p-4(
+    :data='dataList'
+    :pagination='pageService'
+    @page-change='refreshData')
+    template(#columns)
+      a-table-column(data-index='username' title='用户名')
+      a-table-column(data-index='realname' title='姓名')
+      a-table-column(align='center' data-index='createdAt' title='创建时间')
+        template(#cell='{ record }')
+          div {{ filter.dateTimeFormat(record['createdAt']) }}
+      a-table-column(align='center' data-index='updatedAt' title='更新时间')
+        template(#cell='{ record }')
+          div {{ filter.dateTimeFormat(record['updatedAt']) }}
+      a-table-column(align='center')
+        template(#title)
+          a-button(status='success' type='outline' @click='dialog.add = true') 添加管理员
+        template(#cell='{ record }')
+          a-popconfirm(
+            v-if='showDelete'
+            content='是否删除该管理员'
+            @ok='onDelete(record.id)')
+            a-button(status='danger' type='text') 删除
+          a-button(type='text' @click='onResetPwd(record.id)') 重置密码
+          a-button(type='text' @click='onUpdate(record)') 修改
+  //- 编辑模态框
+  a-modal(
+    v-model:visible='dialog.modify'
+    title='管理员姓名'
+    @before-ok='onDialogBeforOk')
+    a-input(v-model.trim='modifyData.name' placeholder='请输入管理员姓名')
+  //- 新增模态框
+  a-modal(v-model:visible='dialog.add' :footer='false' title='添加管理员') 
+    AddAdmin(@close='dialog.add = false' @success='refreshData')
+</template>
+
+<script setup lang="ts">
+import { RequestParams } from '@gopowerteam/http-request'
+import { useRequest } from 'virtual:http-request'
+import { Admin } from '~/http/model'
+import { useFilter } from '~/utils/hook.service'
+import { PageService } from '~/http/extends/page.service'
+import { AdminService } from '~/http/services/admin.service'
+import { Message } from '@arco-design/web-vue'
+import AddAdmin from './components/add-admin.vue'
+
+// 管理员列表
+let dataList = $ref<Admin[]>([])
+
+const adminService = useRequest<AdminService>((service) => service.AdminService)
+const pageService = new PageService()
+
+onMounted(refreshData)
+
+function refreshData() {
+  adminService
+    .findAdmin(
+      new RequestParams({
+        page: pageService,
+      }),
+    )
+    .subscribe({
+      next: (data) => {
+        dataList = data
+      },
+    })
+}
+
+// 大于一个管理员的时候才允许删除
+const showDelete = computed(() => dataList.length > 1)
+
+// 过滤器
+const filter = useFilter()
+
+// 删除管理员
+function onDelete(id: string) {
+  adminService
+    .removeAdmin(
+      new RequestParams({
+        append: { id },
+      }),
+    )
+    .subscribe(refreshData)
+}
+
+// 重置密码
+function onResetPwd(id: string) {
+  adminService
+    .resetAdminPassword(
+      new RequestParams({
+        append: { id },
+      }),
+    )
+    .subscribe({
+      next: () => {
+        Message.success('操作成功')
+      },
+    })
+}
+
+const dialog = $ref({
+  modify: false,
+  add: false,
+})
+
+const modifyData = $ref({
+  id: '',
+  name: '',
+})
+
+// 点击修改
+function onUpdate(data: Admin) {
+  modifyData.id = data.id
+  modifyData.name = data.realname
+  dialog.modify = true
+}
+
+// 修改模态框点击保存时
+function onDialogBeforOk(done: Function) {
+  if (!modifyData.name) {
+    Message.error('请输入管理员姓名')
+    return done(false)
+  }
+  adminService
+    .updateAdmin(
+      new RequestParams({
+        data: {
+          realname: modifyData.name,
+        },
+        append: { id: modifyData.id },
+      }),
+    )
+    .subscribe({
+      next: () => {
+        done()
+        Message.success('修改成功')
+        refreshData()
+      },
+      error: () => done(false),
+    })
+}
+</script>
+
+<route lang="yaml">
+name: admin
+meta:
+  layout: workspace
+  auth:
+    required: true
+    roles:
+      - ADMIN
+  menu:
+    key: root1.admin
+    icon: xxx
+    title: 系统管理员
+</route>
