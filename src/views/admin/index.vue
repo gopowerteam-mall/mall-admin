5b16ff6044f316ab9ab28850cd859c4cf698fb90
黄瑞 <hr837@163.com>
2022-08-26 18:55:53 +0800

    fix(admin page): 改善管理员列表交互体验

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,29 +1,34 @@
 <template lang="pug">
 page-container(title='管理员列表')
-  a-table.p-4(
-    :data='dataList'
-    :pagination='pageService'
-    @page-change='refreshData')
-    template(#columns)
-      a-table-column(data-index='username' title='用户名')
-      a-table-column(data-index='realname' title='姓名')
-      a-table-column(align='center' data-index='createdAt' title='创建时间')
-        template(#cell='{ record }')
-          div {{ dateTimeFormat(record['createdAt']) }}
-      a-table-column(align='center' data-index='updatedAt' title='更新时间')
-        template(#cell='{ record }')
-          div {{ dateTimeFormat(record['updatedAt']) }}
-      a-table-column(align='center')
-        template(#title)
-          a-button(status='success' type='outline' @click='dialog.add = true') 添加管理员
-        template(#cell='{ record }')
-          a-popconfirm(
-            v-if='showDelete'
-            content='是否删除该管理员'
-            @ok='onDelete(record.id)')
-            a-button(status='danger' type='text') 删除
-          a-button(type='text' @click='onResetPwd(record.id)') 重置密码
-          a-button(type='text' @click='onUpdate(record)') 修改
+  a-spin(:loading='loadingStatus' class='!flex')
+    a-table.p-4.flex-1(
+      :data='dataList'
+      :pagination='pageService'
+      @page-change='onPageChange'
+      @page-size-change='onPageSizeChange')
+      template(#columns)
+        a-table-column(data-index='username' title='用户名')
+        a-table-column(data-index='realname' title='姓名')
+        a-table-column(align='center' data-index='createdAt' title='创建时间')
+          template(#cell='{ record }')
+            div {{ dateTimeFormat(record['createdAt']) }}
+        a-table-column(align='center' data-index='updatedAt' title='更新时间')
+          template(#cell='{ record }')
+            div {{ dateTimeFormat(record['updatedAt']) }}
+        a-table-column(align='center')
+          template(#title)
+            a-button(
+              status='success'
+              type='outline'
+              @click='dialog.add = true') 添加管理员
+          template(#cell='{ record }')
+            a-popconfirm(
+              v-if='showDelete'
+              content='是否删除该管理员'
+              @ok='onDelete(record.id)')
+              a-button(status='danger' type='text') 删除
+            a-button(type='text' @click='onResetPwd(record.id)') 重置密码
+            a-button(type='text' @click='onUpdate(record)') 修改
   //- 编辑模态框
   a-modal(
     v-model:visible='dialog.modify'
@@ -40,6 +45,7 @@ import { RequestParams } from '@gopowerteam/http-request'
 import { useRequest } from 'virtual:http-request'
 import { Admin } from '~/http/model'
 import { PageService } from '~/http/extends/page.service'
+import { LoadingService } from '~/http/extends/loading.service'
 import { AdminService } from '~/http/services/admin.service'
 import { Message } from '@arco-design/web-vue'
 import AddAdmin from './components/add-admin.vue'
@@ -50,11 +56,24 @@ let dataList = $ref<Admin[]>([])
 
 const adminService = useRequest<AdminService>((service) => service.AdminService)
 const pageService = new PageService()
+const loadingSerivce = new LoadingService()
 
-onMounted(refreshData)
+// loading状态
+let loadingStatus = $ref(false)
+
+onMounted(() => {
+  loadingSerivce.status.subscribe((value) => (loadingStatus = value))
+  refreshData()
+})
 
 function dateTimeFormat(date: string) {
-  return dayjs(date).format('YYYY-MM-DD')
+  return dayjs(date).format('YYYY-MM-DD HH:mm')
+}
+function onPageChange(index: number) {
+  pageService.update(index, pageService.pageSize).then(refreshData)
+}
+function onPageSizeChange(size: number) {
+  pageService.update(pageService.default.pageIndex, size).then(refreshData)
 }
 
 function refreshData() {
@@ -62,6 +81,7 @@ function refreshData() {
     .findAdmin(
       new RequestParams({
         page: pageService,
+        loading: loadingSerivce,
       }),
     )
     .subscribe({
@@ -80,6 +100,7 @@ function onDelete(id: string) {
     .removeAdmin(
       new RequestParams({
         append: { id },
+        loading: loadingSerivce,
       }),
     )
     .subscribe(refreshData)
@@ -91,11 +112,16 @@ function onResetPwd(id: string) {
     .resetAdminPassword(
       new RequestParams({
         append: { id },
+        loading: loadingSerivce,
       }),
     )
     .subscribe({
-      next: () => {
-        Message.success('操作成功')
+      next: ({ password }) => {
+        Message.success({
+          content: `重置成功，新密码【${password}】,请牢记`,
+          duration: 5000,
+          closable: true,
+        })
       },
     })
 }
