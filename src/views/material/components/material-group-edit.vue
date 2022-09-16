9a96f302f66e44756865eb44c5918ddf880f1dcf
Duang <hr837@163.com>
2022-09-16 18:05:48 +0800

    feat: 添加素材管理

---
+++ src/views/material/components/material-group-edit.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+a-form(
+  :model='form'
+  @submit-success='handleSubmit')
+  a-form-item(field='name' label='分组名称' :rules="[{ required: true, message: '请输入分组名称' }]")
+    a-input(v-model='form.name' allow-clear placeholder='请输入分组名称')
+  a-form-item(content-class='!justify-end')
+    a-button(@click='onCancel') 取消
+    a-button.m-l-4(:loading='saving' html-type='submit' type='primary') 提交
+</template>
+
+<script lang="ts" setup>
+import { Message } from '@arco-design/web-vue'
+import { RequestParams } from '@gopowerteam/http-request'
+import { useModal } from '@gopowerteam/vue-modal'
+import { useRequest } from 'virtual:http-request'
+
+interface PropInterface {
+  id?: string
+  name?: string
+}
+
+const props = defineProps<PropInterface>()
+
+const form = $ref({
+  name: '',
+})
+
+onMounted(() => {
+  if (props.name) form.name = props.name
+})
+
+let saving = $ref(false)
+
+const modal = useModal()
+
+function onCancel() {
+  modal.close(false)
+}
+
+function onSuncess() {
+  saving = false
+  Message.success('添加成功')
+  modal.close(true)
+}
+
+function handleSubmit() {
+  saving = true
+  const service = useRequest((service) => service.MaterialService)
+  if (props.id) {
+    service
+      .updateMaterialGroup(
+        new RequestParams({
+          data: form,
+          append: {
+            id: props.id,
+          },
+        }),
+      )
+      .subscribe({
+        next: onSuncess,
+        error: () => (saving = false),
+      })
+  } else {
+    service.createMaterialGroup(form).subscribe({
+      next: onSuncess,
+      error: () => (saving = false),
+    })
+  }
+}
+</script>
---
+++ src/views/material/index.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+page-container(title='素材分组管理')
+  a-spin(:loading='loadingStatus' class='!flex')
+    a-table.p-4.flex-1( :data='dataList' :expandable="expandable" row-key="id" )
+      template(#columns)
+        a-table-column(data-index='name' title='分组名称')
+        a-table-column(data-index='count' title='素材数量')
+        a-table-column(align='center')
+          template(#title)
+            a-button(status='success' type='outline' @click='openEdit()') 添加分组
+          template(#cell='{ record }')
+            a-button(type='text' @click='openEdit(record)') 修改
+            a-popconfirm(content='是否删除该分组' @ok='onDelete(record.id)') 
+              a-button(status='danger' type='text') 删除
+</template>
+
+<script setup lang="ts">
+import { RequestParams } from '@gopowerteam/http-request'
+import { useRequest } from 'virtual:http-request'
+import { MaterialGroupResponse } from '~/http/model'
+import { PageService } from '~/http/extends/page.service'
+import { LoadingService } from '~/http/extends/loading.service'
+import { useModal } from '@gopowerteam/vue-modal'
+import MaterialGroupEdit from './components/material-group-edit.vue'
+import MaterialGrid from './components/material-grid.vue'
+
+// 列表
+let dataList = $ref<MaterialGroupResponse[]>([])
+
+const materialService = useRequest((service) => service.MaterialService)
+const pageService = new PageService()
+const loadingStatus = ref(false)
+const loadingService = new LoadingService(loadingStatus)
+
+onMounted(refreshData)
+
+function refreshData() {
+  materialService
+    .findMaterialGroup(
+      new RequestParams({
+        page: pageService,
+        loading: loadingService,
+      }),
+    )
+    .subscribe({
+      next: (data) => {
+        dataList = data
+      },
+    })
+}
+
+// 删除
+function onDelete(id: string) {
+  materialService
+    .deleteMaterialGroup(
+      new RequestParams({
+        append: { id },
+        loading: loadingService,
+      }),
+    )
+    .subscribe(refreshData)
+}
+
+const modal = useModal()
+
+function openEdit(record?: MaterialGroupResponse) {
+  const title = record ? '修改分组名称' : '添加分组'
+  modal
+    .open({
+      component: MaterialGroupEdit,
+      width: 500,
+      props: { id: record?.id, name: record?.name },
+      title,
+    })
+    .then((data) => data && refreshData())
+}
+
+const expandable = $ref({
+  title: '展开',
+  width: 100,
+  expandedRowRender: (record: MaterialGroupResponse) => {
+    if (record.count > 0) {
+      return h(MaterialGrid, {
+        groupId: record.id,
+      })
+    }
+  },
+})
+</script>
+
+<route lang="yaml">
+name: material
+meta:
+  layout: workspace
+  auth:
+    required: true
+    roles:
+      - ADMIN
+  menu:
+    key: root1.material
+    icon: xxx
+    title: 素材管理
+</route>
