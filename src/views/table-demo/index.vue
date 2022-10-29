cf598c8b4a78c1a8295e86ea5807c1b3ab34148e
zhuchentong <2037630@163.com>
2022-10-29 08:17:34 +0800

    fix(custom): 更新table demo注释

--- src/views/table-demo/index.vue
+++ src/views/table-demo/index.vue
@@ -1,7 +1,7 @@
 <template>
   <dynamic-table
-    :pagination="pageService"
     row-key="id"
+    :pagination="pageService"
     :load-data="loadData"
     :forms="forms"
     :columns="columns"
@@ -21,20 +21,28 @@ const administratorService = new AdministratorService()
 
 const pageService = new PageService(1, 1)
 
+/**
+ * 加载表格数据
+ * @param param0
+ */
 function loadData({ form, update }: LoadDataParams) {
   administratorService
     .findAdministrator(form, [pageService])
     .then(({ data }) => {
-      update(
-        data.map((x) => {
-          ;(x as any)['phone'] = '18899992222'
-          return x
-        }),
-      )
+      // TEST: phone column显示
+      data.forEach((row: any) => (row['phone'] = '18899992222'))
+      update(data)
     })
 }
 
+/**
+ * 表格配置
+ * 优先级高于columns form优先级
+ * 会覆盖同key配置
+ */
 const forms: FormItemsOptions = [
+  // Select列示例
+  // options支持Promise返回异步数据
   {
     key: 'test1',
     title: 'test1',
@@ -50,21 +58,34 @@ const forms: FormItemsOptions = [
   },
 ]
 
+/**
+ * 表格列配置
+ */
 const columns: TableColumnsOptions = [
   {
     key: 'id',
     title: '名称',
   },
+  // 日期列配置
+  // format 支持预置格式  date datetime week time
+  // format 为函数类型则支持自定义格式如: ()=>"YYYY-MM-DD"
   {
     key: 'createdAt',
     title: 'date',
-    render: (r) => r.date(),
+    render: (r) => r.date({ format: 'datetime' }),
   },
+  // 手机号列配置
+  // safe 手机号脱敏
+  // callable 点击可以拨号
+  // separator 号码分隔符
   {
     key: 'phone',
     title: '手机号',
-    render: (r) => r.phone({ safe: true, callable: true }),
+    render: (r) => r.phone({ safe: true, callable: true, separator: '-' }),
   },
+  // 文本列
+  // 测试表单支持
+  // 测试表单规则
   {
     key: 'username',
     title: '用户名',
@@ -78,6 +99,8 @@ const columns: TableColumnsOptions = [
       render: (r) => r.input({ placeholder: 'asd' }),
     },
   },
+  // 测试表单配置
+  // columns中配置表单会使用对应列的key以及title
   {
     key: 'realname',
     title: '真实姓名',
